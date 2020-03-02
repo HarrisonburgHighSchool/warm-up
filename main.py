53d29fa466c8c609d551d8ec3cd03be02940f142
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2020-03-02 13:57:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-names = ["Akira", "Emmery", "Dia", "Erick", "Nadja"]
-ages = [14, 16, 17, 14, 18, 15]
+import pyglet
+from util import *
 
-for i in range(6):
-  print("Information about " + names[i] + ":")
-  print("Age: " + str(ages[i]))
-  print("Age in dog years: " + str(ages[i] * 7))
-  print("----------------------------------------")
+win = pyglet.window.Window()
+# Load the image & create the sprite
+img = pyglet.image.load('assets/gfx/Inner.png')
+img = img.get_region(x=0, y=384, width=16, height=16)
+
+# Make the sprites
+spr1 = pyglet.sprite.Sprite(img, x = 0, y = 0)
+spr2 = pyglet.sprite.Sprite(img, x = 40, y = 50)
+spr1.scale = 4
+spr2.scale = 4
+
+def update(dt):
+  pass
+
+@win.event
+def on_draw():
+  win.clear()
+  pixelScale()
+
+  spr1.draw()
+  spr2.draw()
+
+pyglet.clock.schedule_interval(update, 0.015)
+pyglet.app.run()
+
